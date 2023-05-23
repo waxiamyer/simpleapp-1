f97c08ace3c578a728741b7527f5e83794fa2e89
Waxia Myer <wasia.maya@gmail.com>
2023-05-23 08:24:04 -0500

    Index fixed

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	</head>
 
 <body> 
-	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action</b></p>
+	<h1>PHP Demo</h1>
+		<p><b>PHP's Demo in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
