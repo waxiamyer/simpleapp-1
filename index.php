d1f9b63858268192a4e940b96cee20dc3a8b3590
waxiamyer <58050316+waxiamyer@users.noreply.github.com>
2023-12-03 10:02:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 	</head>
 
 <body> 
-	<h1>PHP Demo</h1>
+	<h1>PHP Demo part</h1>
 		<p><b>PHP's Demo in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Demo Information</h2> 
 		<p> <?php phpinfo(); ?> </p> 
-        <h2>Happy Monday Test</h2>
+        <h2>Happy Sunday</h2>
         <h2>Last Line</h>$
 	</body> 
 </html>
